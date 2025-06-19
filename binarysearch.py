61668dd1275509d7ad67dc119a5f5ce70986e045
[samuel gregory] <[samuelgregory455@gmail.com]>
2025-06-19 03:26:06 -1200

    binarysearch

---
+++ binarysearch.py
@@ -0,0 +1,20 @@
+def binarysearch(arr, target):
+    """
+    Perform binary search on a sorted array to find the index of the target value.
+
+    :param arr: List[int] - A sorted list of integers.
+    :param target: int - The integer value to search for.
+    :return: int - The index of the target in the array, or -1 if not found.
+    """
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
