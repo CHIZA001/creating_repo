aaf9598ca697151818098e091890e494592cf008
CHIZA <samuelgregory455@Gmail.com>
2025-09-30 15:22:32 +0100

    weather api

--- datatypes.py
+++ datatypes.py
@@ -8,19 +8,20 @@ Alphanumeric-15A@_
 
 """
 #this is a float
-num1 =4.09
-print(type(num1))
-#this is an integer
-num2="2"
-# print(type(num2))
-#adding an integer and float
-print(3+0.93)
-X=2.34
-print(type(X))
-e=2.43
-print(f'hello {e}')
-input("enter your name")
-print({input})
+# num1 =4.09
+# print(type(num1))
+# #this is an integer
+# num2="2"
+# # print(type(num2))
+# #adding an integer and float
+# print(3+0.93)
+# X=2.34
+# print(type(X))
+# e=2.43
+# print(f'hello {e}')
+# input("enter your name")
+# print({input})
+print("hello world")
 
 
 
